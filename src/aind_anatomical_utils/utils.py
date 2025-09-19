6806ba0fa9d565e06016a60e1c9263f4df10628a
Galen Lynch <galen@galenlynch.com>
2025-09-19 15:04:27 -0700

    typing: Fix typing for python 3.13

--- src/aind_anatomical_utils/utils.py
+++ src/aind_anatomical_utils/utils.py
@@ -6,6 +6,6 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def find_indices_equal_to(arr: NDArray, v: Union[int, float, bool]) -> NDArray:
+def find_indices_equal_to(arr: NDArray, v: Union[float, bool]) -> NDArray:
     """Find array indices equal to v"""
     return np.column_stack(np.nonzero(arr == v))
